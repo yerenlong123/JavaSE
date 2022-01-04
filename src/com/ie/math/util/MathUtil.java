e4cbf9a4e887e44e4f92cd86e9d735746053643a
longhun <yr4211261992@163.com>
2022-01-04 14:57:39 +0800

    项目初始化提交

---
+++ src/com/ie/math/util/MathUtil.java
@@ -0,0 +1,14 @@
+package com.ie.math.util;
+
+/**
+ * @author yerenlong
+ * 2022/1/4  14:54
+ */
+public class MathUtil {
+
+
+    public int abs(int num) {
+        return num > 0 ? num : -num;
+    }
+
+}
