c6dffc1ceb17569c00331c7f0e5ad59175373aad
longhun <yr4211261992@163.com>
2022-01-04 18:23:17 +0800

    提交StringUtil文件

---
+++ src/com/ie/math/controller/StringUtil.java
@@ -0,0 +1,14 @@
+package com.ie.math.controller;
+
+/**
+ * @author yerenlong
+ * 2022/1/4  14:54
+ */
+public class StringUtil {
+
+
+    public String abs(int num) {
+        return ""+num;
+    }
+
+}
